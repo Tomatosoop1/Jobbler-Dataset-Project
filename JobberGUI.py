fe520a88c2b1dffb8fc84481119a784398c17e59
Luisssssssssss <100135822+Tomatosoop1@users.noreply.github.com>
2023-03-31 12:44:58 +0200

    v0.3
    So close to working

--- JobberGUI.py
+++ JobberGUI.py
@@ -8,11 +8,13 @@ font = ("Arial", 20)
 layout = [[sg.Text('Jobbler', size=(20, 1), key='-text-', font=font)],
           [sg.Text('An app developed by Luis Wettre\nto find the highest paying job for you!')],
           [sg.Text('Age')],[sg.InputText()],[sg.Text('Gender')],[sg.InputText()],[sg.Text("Country")],[sg.InputText()],
-          [sg.Button('Go!')]+[sg.Button('Cancel')]
+          [sg.Button('Go!')]+[sg.Button('Cancel')],
+          [sg.Text('Best Paying Jobs for you!')],
+          [sg.Output(size=(50,10))]
           
 ]
 
-window = sg.Window('Jobber', layout,size=(300,300))
+window = sg.Window('Jobber', layout,size=(300,500))
 
 while True:
     event, values = window.read()
@@ -22,21 +24,10 @@ while True:
     genderInput = values[1]
     countryInput = values[2]
 
-    age = (df.loc[df['age'] == int(ageInput)])
-    print(age.head(5))
+    #Want to combine these 3 below but don't know how
 
-    gender = (df.loc[df['gender'] == genderInput])
-    print(gender.head(5))
+    result = df.loc[(df['age'] == int(ageInput)) & (df['gender'] == genderInput) & (df['native-country'] == countryInput)]
 
-    country = (df.loc[df['native-country'] == countryInput])
-    print(country.head(5))
-   
-
-'''
-while True:
-    if value in collumn[0] == ageInput and value in collumn[9] == genderInput and value in collumn[13] == countryInput:
-        print()
-
-'''
+    print(result["capital-gain"].nlargest(5))
 
 window.close()
