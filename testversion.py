10beb5324d0fbe736e7a08d117678c88ad8bda95
Luisssssssssss <100135822+Tomatosoop1@users.noreply.github.com>
2023-04-05 14:35:53 +0200

    Huge Changed
    Huge code changed (still not fully working) but also switched datasets to a smaller but better one

--- testversion.py
+++ testversion.py
@@ -3,19 +3,25 @@ import pandas as pd
 import matplotlib as plt
 import numpy as np
 
-df = pd.read_csv('[Python] Data Set Project/Jobbler-Dataset-Project/train.csv')
+df = pd.read_csv('[Python] Data Set Project/Jobbler-Dataset-Project/Salary Data.csv')
 sg.theme('DarkTeal9')
 font = ("Arial", 20)
+
+ageList = [23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53]
+genderList = ["Male","Female"]
+educationList = ['''Bachelor's''','''PhD''','''Master's''']
+jobList = ["Account Manager"]
+experienceList = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
 layout = [[sg.Text('Jobbler', size=(20, 1), key='-text-', font=font)],
           [sg.Text('An app developed by Luis Wettre\nto find the highest paying job for you!\n\nIf you dont want to enter a value in a sertain field leave it blank!')],
-          [sg.Text('Age')],[sg.InputText()],[sg.Text('Gender')],[sg.Combo(["Male","Female"])],[sg.Text("Native-Country")],[sg.Combo(['United-States'])],[sg.Text("Race")],[sg.Combo(["Black","White","Asian"])],[sg.Text("Hours-Per-Week")],[sg.Combo([25,30,35,40,45,50,55,60,65,70,75,80,85,90])],
-          [sg.Text('Occupation')],[sg.Combo(["Exec-managerial","Other-service","Transport-moving"])],[sg.Text('Marital Status')],[sg.Combo(["Maried"])],
+          [sg.Text('Age')],[sg.Combo(ageList)],[sg.Text('Gender')],[sg.Combo(genderList)],[sg.Text('Education')],[sg.Combo(educationList)],[sg.Text('Job Title')],[sg.Combo(jobList)],[sg.Text('Years of Experience')],[sg.Combo(experienceList)],
           [sg.Button('Go!')]+[sg.Button('Cancel')],
           [sg.Text('Best Paying Jobs for you!')],
-          [sg.Output(size=(50,10))]
+          [sg.Output(size=(50,10))] 
 ]
 
-window = sg.Window('Jobbler', layout,size=(700,700))    
+window = sg.Window('Jobbler', layout,size=(500,600))    
 
 while True:
     event, values = window.read()
@@ -23,26 +29,14 @@ while True:
         break
     ageInput = values[0]
     genderInput = values[1]
-    countryInput = values[2]
-    # Add search for occupation, marital status
+    educationInput = values[2]
+    jobInput = values[3]
+    experienceInput = values[4]
 
-    if not ageInput:
-        result = df.loc[(df['gender'] == genderInput) & (df['native-country'] == countryInput)]
-        result_sorted = result.sort_values(by='capital-gain', ascending=False).head(5)
-        print(result_sorted[['occupation', 'capital-gain']])
+   
+    result = df.loc[(df['Age'] == int(ageInput)) & (df['Gender'] == genderInput) & (df['Education Level'] == educationInput) & (df['Job Title'] == jobInput) & (df['Years of Experience'] == experienceInput)]
+    result_sorted = result.sort_values(by='Salary', ascending=False).head(5)
+    print(result_sorted[['Job Title', 'Salary']])
 
-    else:
-        result = df.loc[(df['age'] == int(ageInput)) & (df['gender'] == genderInput) & (df['native-country'] == countryInput)]
-        result_sorted = result.sort_values(by='capital-gain', ascending=False).head(5)
-        print(result_sorted[['occupation', 'capital-gain']])
-
-def CheckForInputs():
-    while True:
-        if not ageInput:
-            continue
-        if not genderInput:
-            continue
-        if not countryInput:
-            continue
 
 window.close()
